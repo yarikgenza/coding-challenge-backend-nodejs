c07465daba2e40d33ca7c3991c3a16aa0681ec0c
Yarik Henza <gsmeter@gmail.com>
2020-08-21 10:53:04 +0300

    fix wrong model path patterns

--- src/models/index.js
+++ src/models/index.js
@@ -19,7 +19,7 @@ if (config.url) {
 fs
   .readdirSync(__dirname)
   .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-8) === 'model.js');
   })
   .forEach(file => {
     const model = require(path.join(__dirname, file))(sequelize, Sequelize);
